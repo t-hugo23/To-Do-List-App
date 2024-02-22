91fc17874b482b9309e0a7bbfa360575cd55c83e
Hugo Tobar <x3rox23@gmail.com>
2024-02-21 19:30:07 -0800

    incorrect syntax was used

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const form = document.getElementById('todo-form');
-const input = document.getElementById('toso-input');
+const input = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    const taskText = input.ariaValueMax.trim();
+    const taskText = input.value.trim();
     if (taskText !== '') {
         addTask(taskText);
         input.value = '';
